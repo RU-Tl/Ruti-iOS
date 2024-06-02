ebfe4c07f9dbaab562e92042285c59dcd74a7ee5
안이연 <lxxyeon@gmail.com>
2024-06-02 22:47:28 +0900

    Resource 추가

---
+++ Ruti/Ruti/View/RutineAlarm/RutineStartConfirmVC.swift
@@ -0,0 +1,29 @@
+//
+//  RutineStartConfirmVC.swift
+//  Ruti
+//
+//  Created by leeyeon2 on 5/25/24.
+//
+
+import UIKit
+
+class RutineStartConfirmVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
