9e64a4fe53b182acc7e0338341c64871b277d6e8
안이연 <lxxyeon@gmail.com>
2024-06-09 22:28:19 +0900

    API 통신 구현을 위한 설정

--- Ruti/Ruti/Service/APIConfig.swift
+++ Ruti/Ruti/Service/APIConfig.swift
@@ -9,13 +9,14 @@ import Foundation
 import Alamofire
 
 struct APIConfig {
-    static let baseURL = ""
+    static let baseURL = "http://49.50.173.179:8080"
     
     static var headers: HTTPHeaders = ["Accept" : "application/json, application/javascript, text/javascript, text/json",
                                 "Content-Type" : "application/json; charset=UTF-8"]
-//    static let authHeaders: HTTPHeaders = ["Accept" : "application/json, application/javascript, text/javascript, text/json",
-//                                    "Content-Type" : "application/json; charset=UTF-8",
-//                                           "Authorization" : "Bearer \(UserInfo.token))"]
+    
+    static let authHeaders: HTTPHeaders = ["Accept" : "application/json, application/javascript, text/javascript, text/json",
+                                    "Content-Type" : "application/json; charset=UTF-8",
+                                           "Authorization" : "Bearer \(UserInfo.token))"]
 
     static let multiPartHeaders: HTTPHeaders = ["Content-Type" : "multipart/form-data"]
 }
