ef1a4893906b7158c570799d52aa7e8f981012eb
안이연 <lxxyeon@gmail.com>
2024-06-10 14:23:46 +0900

    [화면] 마이페이지 개발

---
+++ Ruti/Ruti/Model/RankUser.swift
@@ -0,0 +1,8 @@
+//
+//  RankUser.swift
+//  Ruti
+//
+//  Created by leeyeon2 on 6/10/24.
+//
+
+import Foundation
--- Ruti/Ruti/View/NavigationVC/MyPageVC.swift
+++ Ruti/Ruti/View/NavigationVC/MyPageVC.swift
@@ -8,22 +8,112 @@
 import UIKit
 
 class MyPageVC: UIViewController {
-
+    @IBOutlet weak var nameLabel2: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imgBaseView: UIView!
+    @IBOutlet weak var raceView: UIView!
+    
+    @IBOutlet weak var racetitle1: UILabel!
+    @IBOutlet weak var racetitle2: UILabel!
+    
+    @IBOutlet weak var progressBase: UIView!
+    @IBOutlet weak var progressBar: UIView!
+    
+    @IBOutlet weak var lv1: UILabel!
+    @IBOutlet weak var lv2: UILabel!
+    @IBOutlet weak var lv3: UILabel!
+    @IBOutlet weak var lv4: UILabel!
+    
+    @IBOutlet weak var 로그인타입: UILabel!
+    @IBOutlet weak var myPageTable: UITableView!
+    
+    var race = 30
+    var level = "루티니"
+    var levelList = ["루꼬미", "루티니", "루틴 달인", "루틴 만렙"]
+    var socialType = "카카오"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        nameLabel.text = "\(UserInfo.name)"
+        racetitle1.text = "루틴 달성률 상위 \(race)%의"
+        racetitle2.text = "\(level)에요!"
+        
+        guard let text = self.racetitle2.text else { return }
+        let attributeString = NSMutableAttributedString(string: text)
+        attributeString.addAttribute(.foregroundColor, value: UIColor.init(hexCode: "#FFBA58"), range: (text as NSString).range(of: "\(level)"))
+        self.racetitle2.attributedText = attributeString
+        
+        로그인타입.text = "\(socialType)로 로그인 하셨어요"
+        guard let text = self.로그인타입.text else { return }
+        let attributeString2 = NSMutableAttributedString(string: text)
+        attributeString2.addAttribute(.foregroundColor, value: UIColor.init(hexCode: "#FEE500"), range: (text as NSString).range(of: "\(socialType)"))
+        self.로그인타입.attributedText = attributeString2
+        
+        lv1.text = levelList[0]
+        lv2.text = levelList[1]
+        lv3.text = levelList[2]
+        lv4.text = levelList[3]
+        
+        nameLabel.font = UIFont.body1()
+        nameLabel2.font = UIFont.body1()
+        racetitle1.font = UIFont.body1()
+        racetitle2.font = UIFont.body1()
+        
+        lv1.font = UIFont.caption1()
+        lv2.font = UIFont.caption1()
+        lv3.font = UIFont.caption1()
+        lv4.font = UIFont.caption1()
+        
+        로그인타입.font = UIFont.caption1()
+        imgBaseView.layer.cornerRadius = 50
+        imgBaseView.backgroundColor = .clear
+        imgBaseView.layer.borderWidth = 1
+        imgBaseView.layer.borderColor = UIColor(hexCode: "B5DCFF").cgColor
+        raceView.layer.cornerRadius = 15
+        raceView.layer.borderWidth = 1
+        raceView.layer.borderColor = UIColor.black.cgColor
+        progressBase.layer.cornerRadius = 5
+        progressBar.layer.cornerRadius = 5
     }
-    */
+}
 
+extension MyPageVC: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(indexPath.row == 0){
+//            cell.textLabel?.text = "루틴 알림 설정"
+        }else if(indexPath.row == 0){
+//            cell.textLabel?.text = "로그아웃"
+        }else{
+//            cell.textLabel?.text = "계정 삭제"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageCell")!
+        
+        cell.textLabel?.font = UIFont.body3()
+        if(indexPath.row == 0){
+            cell.textLabel?.text = "루틴 알림 설정"
+        }else if(indexPath.row == 1){
+            cell.textLabel?.text = "로그아웃"
+        }else{
+            cell.textLabel?.text = "계정 삭제"
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
 }
