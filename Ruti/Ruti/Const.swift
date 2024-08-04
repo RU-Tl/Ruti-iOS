fa49bca5ee8ed61611d498222752b6c57a3b4e35
안이연 <lxxyeon@gmail.com>
2024-08-04 20:54:43 +0900

    1. 공통 const 정리 - 폰트, 색상

--- Ruti/Ruti/Const.swift
+++ Ruti/Ruti/Const.swift
@@ -5,25 +5,85 @@
 //  Created by leeyeon2 on 5/22/24.
 //
 
-import Foundation
-
-
+import UIKit
 
 struct CustomColor {
-    //fix
-    struct Category{
-        static let health = "#54ADFF" //blue
-        static let reading = "#CF80FF"//pink
-        static let develop = "#FFBA58"//yellow
-    }
-    
+    // Basic Color
+    static let clear_white = "#FFFFFF"
+    static let white = "#FAFAFA"
+    static let white_gray = "#BDBDBD"
+    static let light_gray = "#9E9E9E"
+    static let dark_gray = "#757575"
+    static let deep_dark_gray = "#3E3E3E"
+    static let black_gray = "#333333"
     static let background_black = "#232323"
+    static let black = "#111111"
+    
     static let light_blue = "#B5DCFF"
     
-    //
+    // Main Color
+    struct Category{
+        static let EXERCISE = "#54ADFF" //blue
+        static let READING = "#CF80FF" //pink
+        static let DEVELOPMENT = "#FFBA58" //yellow
+    }
     
-
-    static let background3 = "#b1e7e1"
-    static let background4 = "#b1e7e1"
+    // Specific Color
+    static let kakao = "#FEE500"
 }
 
+extension UIFont {
+    enum wantedFontName: String {
+        case Bold = "WantedSans-Bold"
+        case SemiBold = "WantedSans-SemiBold"
+        case Black = "WantedSans-Black"
+        case ExtraBlack = "WantedSans-ExtraBlack"
+        case ExtraBold = "WantedSans-ExtraBold"
+        case Medium = "WantedSans-Medium"
+        case Regular = "WantedSans-Regular"
+    }
+    
+    class func h1() -> UIFont {
+        return UIFont(name: wantedFontName.Bold.rawValue, size: 28)!
+    }
+    
+    class func h2() -> UIFont {
+        return UIFont(name: wantedFontName.SemiBold.rawValue, size: 24)!
+    }
+    
+    class func h3() -> UIFont {
+        return UIFont(name: wantedFontName.Bold.rawValue, size: 20)!
+    }
+    
+    class func h4() -> UIFont {
+        return UIFont(name: wantedFontName.SemiBold.rawValue, size: 20)!
+    }
+    
+    class func subTitle() -> UIFont {
+        return UIFont(name: wantedFontName.Regular.rawValue, size: 20)!
+    }
+    
+    class func body1() -> UIFont {
+        return UIFont(name: wantedFontName.SemiBold.rawValue, size: 18)!
+    }
+    
+    class func body2() -> UIFont {
+        return UIFont(name: wantedFontName.Regular.rawValue, size: 18)!
+    }
+    
+    class func body3() -> UIFont {
+        return UIFont(name: wantedFontName.Regular.rawValue, size: 16)!
+    }
+    
+    class func body4() -> UIFont {
+        return UIFont(name: wantedFontName.SemiBold.rawValue, size: 14)!
+    }
+    
+    class func caption1() -> UIFont {
+        return UIFont(name: wantedFontName.Regular.rawValue, size: 14)!
+    }
+    
+    class func weekly() -> UIFont {
+        return UIFont(name: wantedFontName.SemiBold.rawValue, size: 18)!
+    }
+}
