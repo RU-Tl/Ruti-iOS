a5f93d794e4cb52ffe88027f8fa88134a4007fe4
안이연 <lxxyeon@gmail.com>
2024-08-17 16:47:29 +0900

    1. 마이페이지 사이드 메뉴 개발

--- Ruti/Ruti/View/NavigationVC/MyPageVC.swift
+++ Ruti/Ruti/View/NavigationVC/MyPageVC.swift
@@ -45,13 +45,13 @@ class MyPageVC: UIViewController {
         
         guard let text = self.racetitle2.text else { return }
         let attributeString = NSMutableAttributedString(string: text)
-        attributeString.addAttribute(.foregroundColor, value: UIColor.init(hexCode: "#FFBA58"), range: (text as NSString).range(of: "\(level)"))
+        attributeString.addAttribute(.foregroundColor, value: UIColor.init(hexCode: CustomColor.Category.DEVELOPMENT), range: (text as NSString).range(of: "\(level)"))
         self.racetitle2.attributedText = attributeString
         
         로그인타입.text = "\(socialType)로 로그인 하셨어요"
         guard let text = self.로그인타입.text else { return }
         let attributeString2 = NSMutableAttributedString(string: text)
-        attributeString2.addAttribute(.foregroundColor, value: UIColor.init(hexCode: "#FEE500"), range: (text as NSString).range(of: "\(socialType)"))
+        attributeString2.addAttribute(.foregroundColor, value: UIColor.init(hexCode: CustomColor.kakao), range: (text as NSString).range(of: "\(socialType)"))
         self.로그인타입.attributedText = attributeString2
         
         lv1.text = levelList[0]
@@ -73,7 +73,7 @@ class MyPageVC: UIViewController {
         imgBaseView.layer.cornerRadius = 50
         imgBaseView.backgroundColor = .clear
         imgBaseView.layer.borderWidth = 1
-        imgBaseView.layer.borderColor = UIColor(hexCode: "B5DCFF").cgColor
+        imgBaseView.layer.borderColor = UIColor(hexCode: CustomColor.light_blue).cgColor
         raceView.layer.cornerRadius = 15
         raceView.layer.borderWidth = 1
         raceView.layer.borderColor = UIColor.black.cgColor
@@ -85,13 +85,65 @@ class MyPageVC: UIViewController {
 extension MyPageVC: UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var alert = UIAlertController()
+        
+        // 1. 루틴 알림 설정
         if(indexPath.row == 0){
-//            cell.textLabel?.text = "루틴 알림 설정"
-        }else if(indexPath.row == 0){
-//            cell.textLabel?.text = "로그아웃"
+            guard
+                let settingURL = URL(string: UIApplication.openSettingsURLString),
+                UIApplication.shared.canOpenURL(settingURL)
+            else { return }
+            UIApplication.shared.open(settingURL, options: [:])
+        }else if(indexPath.row == 1){
+            // 2. 로그아웃
+            alert = UIAlertController(title:"로그아웃 하시겠습니까?",
+                                      message: "",
+                                      preferredStyle: UIAlertController.Style.alert)
+            self.present(alert,animated: true,completion: nil)
+            let actionCancel = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            let actionConfirm = UIAlertAction(title: "확인", style: .default, handler: {_ in
+                UIViewController.changeRootVCToSignIn()
+            })
+            alert.addAction(actionConfirm)
+            alert.addAction(actionCancel)
         }else{
-//            cell.textLabel?.text = "계정 삭제"
+            // 3. 계정 삭제
+            // 로그인 화면(SignInViewController) 으로 이동
+            alert = UIAlertController(title:"계정 탈퇴하시겠습니까?",
+                                      message: "지난 루틴 기록이 모두 사라집니다.",
+                                      preferredStyle: UIAlertController.Style.alert)
+            self.present(alert,animated: true,completion: nil)
+            
+            let actionConfirm = UIAlertAction(title: "확인", style: .default, handler: {_ in
+                //
+                let request = APIRequest(method: .delete,
+                                         path: "/member" + "/\(UserInfo.memberId)",
+                                         param: nil,
+                                         headers: APIConfig.authHeaders)
+                APIService.shared.perform(request: request,
+                                          completion: { [self] (result) in
+                    AlertView.showAlert(title: "회원 탈퇴가 완료되었습니다.",
+                                        message: "로그인 화면으로 이동합니다.",
+                                        viewController: self,
+                                        dismissAction: self.deleteAccountMenu)
+                })
+            })
+            let actionCancel = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            alert.addAction(actionConfirm)
+            alert.addAction(actionCancel)
         }
+    }
+    
+    func deleteAccountMenu() {
+        //UserInfo data init
+        
+        //Userdefalut 삭제
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key.description)
+        }
+        //로그인 화면 이동
+        UIViewController.changeRootVCToSignIn()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
